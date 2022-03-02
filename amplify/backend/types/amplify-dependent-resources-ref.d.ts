d57869ad438e8510262dbc61434770b8b027e7f2
Laurence Montgomery <admin@lmontgomery.ca>
2022-03-01 22:22:24 -0500

    2

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "vgm": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
